f94acb210ce89f90c4ef3b7e886ec91205bfa696
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2022-09-01 11:31:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cgitb import reset
 import sys
 import time
 import random
@@ -6,6 +7,23 @@ import socket
 import sqlite3
 from unicodedata import name
 from obswebsocket import obsws, requests  # noqa: E402
+import pyaudio
+import struct
+import math
+
+INITIAL_TAP_THRESHOLD = 0.25
+FORMAT = pyaudio.paInt16 
+SHORT_NORMALIZE = (1.0/32768.0)
+CHANNELS = 2
+RATE = 44100  
+INPUT_BLOCK_TIME = 0.05
+INPUT_FRAMES_PER_BLOCK = int(RATE*INPUT_BLOCK_TIME)
+# if we get this many noisy blocks in a row, increase the threshold
+OVERSENSITIVE = 15.0/INPUT_BLOCK_TIME                    
+# if we get this many quiet blocks in a row, decrease the threshold
+UNDERSENSITIVE = 120.0/INPUT_BLOCK_TIME 
+# if the noise was longer than this many blocks, it's not a 'tap'
+MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME
 
 class ScenesDb:
     def __init__(self, dbfile):
@@ -26,7 +44,96 @@ class ScenesDb:
     def get20Last(self):
         self.cursor.execute('''SELECT * FROM scenes ORDER BY id DESC LIMIT 20''')
         return self.cursor.fetchall()
-    
+
+def get_rms( block ):
+    # RMS amplitude is defined as the square root of the 
+    # mean over time of the square of the amplitude.
+    # so we need to convert this string of bytes into 
+    # a string of 16-bit samples...
+
+    # we will get one short out for each 
+    # two chars in the string.
+    count = len(block)/2
+    format = "%dh"%(count)
+    shorts = struct.unpack( format, block )
+
+    # iterate over the block.
+    sum_squares = 0.0
+    for sample in shorts:
+        # sample is a signed short in +/- 32768. 
+        # normalize it to 1.0
+        n = sample * SHORT_NORMALIZE
+        sum_squares += n*n
+
+    return math.sqrt( sum_squares / count )
+
+class TapTester(object):
+    def __init__(self):
+        self.pa = pyaudio.PyAudio()
+        self.stream = self.open_mic_stream()
+        self.tap_threshold = INITIAL_TAP_THRESHOLD
+        self.noisycount = MAX_TAP_BLOCKS+1 
+        self.quietcount = 0 
+        self.errorcount = 0
+
+    def stop(self):
+        self.stream.close()
+
+    def find_input_device(self):
+        device_index = None            
+        for i in range( self.pa.get_device_count() ):     
+            devinfo = self.pa.get_device_info_by_index(i)   
+            print( "Device %d: %s"%(i,devinfo["name"]) )
+
+            for keyword in ["mic","input"]:
+                if keyword in devinfo["name"].lower():
+                    print( "Found an input: device %d - %s"%(i,devinfo["name"]) )
+                    device_index = i
+                    return device_index
+
+        if device_index == None:
+            print( "No preferred input found; using default input device." )
+
+        return device_index
+
+    def open_mic_stream( self ):
+        device_index = self.find_input_device()
+
+        stream = self.pa.open(   format = FORMAT,
+                                 channels = CHANNELS,
+                                 rate = RATE,
+                                 input = True,
+                                 input_device_index = device_index,
+                                 frames_per_buffer = INPUT_FRAMES_PER_BLOCK)
+
+        return stream
+
+    def tapDetected(self): #DETECTED
+        return True
+
+    def listen(self):
+        try:
+            block = self.stream.read(INPUT_FRAMES_PER_BLOCK)
+        except IOError as e:
+            # dammit. 
+            self.errorcount += 1
+            print( "(%d) Error recording: %s"%(self.errorcount,e) )
+            self.noisycount = 1
+            return
+
+        amplitude = get_rms( block )
+        if amplitude > self.tap_threshold:
+            # noisy block
+            self.quietcount = 0
+            self.noisycount += 1
+        else:            
+            # quiet block.
+
+            if 1 <= self.noisycount <= MAX_TAP_BLOCKS:
+                self.tapDetected()
+            self.noisycount = 0
+            self.quietcount += 1
+
 def nowTime():
     """Format : [hh:mm:ss]"""
     currentTime=time.strftime("%H:%M:%S")
@@ -83,15 +190,24 @@ def randomChangePi():
     ws.call(requests.SetCurrentScene(name))
     return name
 
+def testPiano():
+    # detect if piano is playing in microphone
+    result=False
+    return result
+
+def testApplause():
+    # detect if applause is heard in microphone
+    for i in range(1000):
+        result=tt.listen()
+    return result
+
 def testStatus():
     # 0 = all, 1 = obj, 2 = pu, 3 = pi
     # TO DO : test of situation by default return 0 (no reason)
     status=0
-    if True: #RandomAll
-        status=0
-    elif True: #RandomObj (no piano, no applause)
+    if te: #RandomObj (no piano, no applause)
         status=1
-    elif True: #RandomPu (applause detected)
+    elif testApplause(): #RandomPu (applause detected)
         status=2
     elif True: #RandomPi (piano detected)
         status=3
@@ -126,6 +242,7 @@ def loopRandomChange():
             time.sleep(1)   
 
 if __name__ == '__main__':
+    tt = TapTester()
     ScenesDb = ScenesDb('scenes.db')
     socket.getaddrinfo('localhost', 8080)
     logging.basicConfig(level=logging.INFO)
