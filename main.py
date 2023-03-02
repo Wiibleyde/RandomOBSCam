0d33cdbd76ffe0d9d773b1de9eab50b89f05c82a
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-03-02 21:05:30 +0100

    update with logs fix and tentative of switch scene fix
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- main.py
+++ main.py
@@ -35,31 +35,30 @@ class Config:
 class Logs:
     def __init__(self, filename):
         self.filename = filename
-        self.logs = []
         if not os.path.exists(self.filename):
             self.create()
 
     def create(self):
         with open(self.filename, "w") as f:
-            json.dump(self.logs, f)
+            json.dump([], f)
 
     def addInfo(self, message):
         stringLog = f"[INFO] {time.strftime('%d/%m/%Y %H:%M:%S')} - {message}"
         print(stringLog)
-        with open(self.filename, "a") as f:
-            f.write(stringLog)
+        with open(self.filename, "a",encoding='utf8') as f:
+            f.write(stringLog+"\n")
 
     def addError(self, message):
         stringLog = f"[ERROR] {time.strftime('%d/%m/%Y %H:%M:%S')} - {message}"
         print(stringLog)
-        with open(self.filename, "a") as f:
-            f.write(stringLog)
+        with open(self.filename, "a",encoding='utf8') as f:
+            f.write(stringLog+"\n")
 
     def addWarning(self, message):
         stringLog = f"[WARNING] {time.strftime('%d/%m/%Y %H:%M:%S')} - {message}"
         print(stringLog)
-        with open(self.filename, "a") as f:
-            f.write(stringLog)
+        with open(self.filename, "a",encoding='utf8') as f:
+            f.write(stringLog+"\n")
 
 class OBS:
     def __init__(self, config, logs):
@@ -101,7 +100,6 @@ class OBS:
             self.logs.addError(f"Get current scene failed : {e}")
 
     def setCurrentScene(self, sceneName):
-        print(sceneName)
         try:
             self.obs.call(obswebsocket.requests.SetCurrentScene(sceneName))
             self.logs.addInfo(f"Set current scene to {sceneName} successful")
@@ -143,19 +141,19 @@ def autoCam():
             # chose a random scene
             logs.addInfo("Changement de scène aléatoire car pas priorité")
             sceneSize = len(validScenes)
-            randomScene = validScenes[random.randint(0, sceneSize)]
+            randomScene = validScenes[random.randint(0, sceneSize-1)]
             obs.setCurrentScene(randomScene["sceneName"])
         elif needed == 1:
             # chose a random scene only in scene scenes
             logs.addInfo("Changement de scène aléatoire dans les scènes de scène")
             sceSceneSize = len(sceneScenes)
-            randomScene = sceneScenes[random.randint(0, sceSceneSize)]
+            randomScene = sceneScenes[random.randint(0, sceSceneSize-1)]
             obs.setCurrentScene(randomScene["sceneName"])
         elif needed == 2:
             # chose a random scene only in public scenes
             logs.addInfo("Changement de scène aléatoire dans les scènes du public")
             pubSceneSize = len(publicScenes)
-            randomScene = publicScenes[random.randint(0, pubSceneSize)]
+            randomScene = publicScenes[random.randint(0, pubSceneSize-1)]
             obs.setCurrentScene(randomScene["sceneName"])
         elif needed == 3:
             # chose a random scene only in piano scenes
@@ -171,6 +169,6 @@ def autoCam():
 
 if __name__ == "__main__":
     config = Config("config.json")
-    logs = Logs("logs.txt")
+    logs = Logs("logs.log")
     obs = OBS(config, logs)
     autoCam()
